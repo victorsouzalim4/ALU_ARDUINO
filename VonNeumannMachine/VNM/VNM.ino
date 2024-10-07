534812fd6ec2e13a0d8d0340d29b88aff96491b6
Victor Souza Lima <victorsouzalima2064@gmail.com>
2024-10-07 19:43:26 -0300

    code compactation

--- VonNeumannMachine/VNM/VNM.ino
+++ VonNeumannMachine/VNM/VNM.ino
@@ -108,170 +108,46 @@ class VonNeumannMachine{
   }
 
   private:
-  String operate(String var0, String var1, char opCode){
-    String result = "";
-
-    switch(opCode){
-      case '0':
-        for(int i = 0; i < 4; i++){
-          result += op0(var0[i], var1[i]); 
-        }
-        break;
-      case '1':
-        for(int i = 0; i < 4; i++){
-          result += op1(var0[i], var1[i]); 
-        }
-        break;
-      case '2':
-        for(int i = 0; i < 4; i++){
-          result += op2(var0[i], var1[i]); 
-        }
-        break;
-      case '3':
-        for(int i = 0; i < 4; i++){
-          result += op3(var0[i], var1[i]); 
-        }
-        break;
-      case '4':
-        for(int i = 0; i < 4; i++){
-          result += op4(var0[i], var1[i]); 
-        }
-        break;
-      case '5':
-        for(int i = 0; i < 4; i++){
-          result += op5(var0[i], var1[i]); 
-        }
-        break;
-      case '6':
-        for(int i = 0; i < 4; i++){
-          result += op6(var0[i], var1[i]); 
-        }
-        break;
-      case '7':
-        for(int i = 0; i < 4; i++){
-          result += op7(var0[i], var1[i]); 
-        }
-        break;
-      case '8':
-        for(int i = 0; i < 4; i++){
-          result += op8(var0[i], var1[i]); 
-        }
-        break;
-      case '9':
-        for(int i = 0; i < 4; i++){
-          result += op9(var0[i], var1[i]); 
-        }
-        break;
-      case 'A':
-        for(int i = 0; i < 4; i++){
-          result += opA(var0[i], var1[i]); 
-        }
-        break;
-      case 'B':
-        for(int i = 0; i < 4; i++){
-          result += opB(var0[i], var1[i]); 
-        }
-        break;
-      case 'C':
-        for(int i = 0; i < 4; i++){
-          result += opC(var0[i], var1[i]); 
-        }
-        break;
-      case 'D':
-        for(int i = 0; i < 4; i++){
-          result += opD(var0[i], var1[i]); 
-        }
-        break;
-      case 'E':
-        for(int i = 0; i < 4; i++){
-          result += opE(var0[i], var1[i]); 
-        }
-        break;
-      case 'F':
-        for(int i = 0; i < 4; i++){
-          result += opF(var0[i], var1[i]); 
-        }
-        break;
-    }
-
-    return result;
-  }
 
   String fromHexaToBinary(char value){
     switch(value){
-      case '0':
-        return "0000";
-      case '1':
-        return "0001";
-      case '2':
-        return "0010";
-      case '3':
-        return "0011";
-      case '4':
-        return "0100";
-      case '5':
-        return "0101";
-      case '6':
-        return "0110";
-      case '7':
-        return "0111";
-      case '8':
-        return "1000";
-      case '9':
-        return "1001";
-      case 'A':
-        return "1010";
-      case 'B':
-        return "1011";
-      case 'C':
-        return "1100";
-      case 'D':
-        return "1101";
-      case 'E':
-        return "1110";
-      case 'F':
-        return "1111";
+      case '0': return "0000";
+      case '1': return "0001";
+      case '2': return "0010";
+      case '3': return "0011";
+      case '4': return "0100";
+      case '5': return "0101";
+      case '6': return "0110";
+      case '7': return "0111";
+      case '8': return "1000";
+      case '9': return "1001";
+      case 'A': return "1010";
+      case 'B': return "1011";
+      case 'C': return "1100";
+      case 'D': return "1101";
+      case 'E': return "1110";
+      case 'F': return "1111";
     }
-
   }
 
   char fromBinaryToHexa(String value){
-   if(value.compareTo("0000") == 0){
-      return '0';
-   }else if(value.compareTo("0001") == 0){
-       return '1';
-   }else if(value.compareTo("0010") == 0){
-       return '2';
-   }else if(value.compareTo("0011") == 0){
-       return '3';
-   }else if(value.compareTo("0100") == 0){
-       return '4';
-   }else if(value.compareTo("0101") == 0){
-       return '5';
-   }else if(value.compareTo("0110") == 0){
-       return '6';
-   }else if(value.compareTo("0111") == 0){
-       return '7';
-   }else if(value.compareTo("1000") == 0){
-       return '8';
-   }else if(value.compareTo("1001") == 0){
-       return '9';
-   }else if(value.compareTo("1010") == 0){
-       return 'A';
-   }else if(value.compareTo("1011") == 0){
-       return 'B';
-   }else if(value.compareTo("1100") == 0){
-       return 'C';
-   }else if(value.compareTo("1101") == 0){
-       return 'D';
-   }else if(value.compareTo("1110") == 0){
-       return 'E';
-   }else if(value.compareTo("1111") == 0){
-       return 'F';
-   }else{
-    return '0';
-   }
-   
+   if(value.compareTo("0000") == 0){ return '0';
+   }else if(value.compareTo("0001") == 0){ return '1';
+   }else if(value.compareTo("0010") == 0){ return '2';
+   }else if(value.compareTo("0011") == 0){ return '3';
+   }else if(value.compareTo("0100") == 0){ return '4';
+   }else if(value.compareTo("0101") == 0){ return '5';
+   }else if(value.compareTo("0110") == 0){ return '6';
+   }else if(value.compareTo("0111") == 0){ return '7';
+   }else if(value.compareTo("1000") == 0){ return '8';
+   }else if(value.compareTo("1001") == 0){ return '9';
+   }else if(value.compareTo("1010") == 0){ return 'A';
+   }else if(value.compareTo("1011") == 0){ return 'B';
+   }else if(value.compareTo("1100") == 0){ return 'C';
+   }else if(value.compareTo("1101") == 0){ return 'D';
+   }else if(value.compareTo("1110") == 0){ return 'E';
+   }else if(value.compareTo("1111") == 0){ return 'F';
+   }else{ return '0';}
   }
 
   char andGate(char a, char b){
@@ -305,56 +181,46 @@ class VonNeumannMachine{
     return result == 0 ? '0' : '1';
   }
 
-  char op0(char a, char b){
-    return orGate(a, notGate(a));
-  }
-  char op1(char a, char b){
-    return orGate(a, notGate(b));
-  }
-  char op2(char a, char b){
-    return andGate(a, a);
-  }
-  char op3(char a, char b){
-    return xorGate(notGate(a), notGate(b));
-  }
-  char op4(char a, char b){
-    return notGate(andGate(a, b));
-  }
-  char op5(char a, char b){
-    return notGate(a);
-  }
-  char op6(char a, char b){
-    return andGate(a, notGate(b));
-  }
-  char op7(char a, char b){
-    return orGate(notGate(a), notGate(b));
-  }
-  char op8(char a, char b){
-    return xorGate(a, b);
-  }
-  char op9(char a, char b){
-    return andGate(a, notGate(a));
-  }
-  char opA(char a, char b){
-    return andGate(b, b);
-  }
-  char opB(char a, char b){
-    return andGate(a, b);
-  }
-  char opC(char a, char b){
-    return notGate(b);
-  }
-  char opD(char a, char b){
-    return notGate(andGate(notGate(a), b));
-  }
-  char opE(char a, char b){
-    return orGate(a, b);
-  }
-  char opF(char a, char b){
-    return andGate(notGate(a), b);
+  String operate(String var0, String var1, char opCode){
+    String result = "";
+    switch(opCode){
+      case '0': for (int i = 0; i < 4; i++) result += op0(var0[i], var1[i]); break;
+      case '1': for (int i = 0; i < 4; i++) result += op1(var0[i], var1[i]); break;
+      case '2': for (int i = 0; i < 4; i++) result += op2(var0[i], var1[i]); break;
+      case '3': for (int i = 0; i < 4; i++) result += op3(var0[i], var1[i]); break;
+      case '4': for (int i = 0; i < 4; i++) result += op4(var0[i], var1[i]); break;
+      case '5': for (int i = 0; i < 4; i++) result += op5(var0[i], var1[i]); break;
+      case '6': for (int i = 0; i < 4; i++) result += op6(var0[i], var1[i]); break;
+      case '7': for (int i = 0; i < 4; i++) result += op7(var0[i], var1[i]); break;
+      case '8': for (int i = 0; i < 4; i++) result += op8(var0[i], var1[i]); break;
+      case '9': for (int i = 0; i < 4; i++) result += op9(var0[i], var1[i]); break;
+      case 'A': for (int i = 0; i < 4; i++) result += opA(var0[i], var1[i]); break;
+      case 'B': for (int i = 0; i < 4; i++) result += opB(var0[i], var1[i]); break;
+      case 'C': for (int i = 0; i < 4; i++) result += opC(var0[i], var1[i]); break;
+      case 'D': for (int i = 0; i < 4; i++) result += opD(var0[i], var1[i]); break;
+      case 'E': for (int i = 0; i < 4; i++) result += opE(var0[i], var1[i]); break;
+      case 'F': for (int i = 0; i < 4; i++) result += opF(var0[i], var1[i]); break;
+    }
+    return result;
   }
 
-};
+  char op0(char a, char b) { return orGate(a, notGate(a)); }
+  char op1(char a, char b) { return orGate(a, notGate(b)); }
+  char op2(char a, char b) { return andGate(a, a); }
+  char op3(char a, char b) { return xorGate(notGate(a), notGate(b)); }
+  char op4(char a, char b) { return notGate(andGate(a, b)); }
+  char op5(char a, char b) { return notGate(a); }
+  char op6(char a, char b) { return andGate(a, notGate(b)); }
+  char op7(char a, char b) { return orGate(notGate(a), notGate(b)); }
+  char op8(char a, char b) { return xorGate(a, b); }
+  char op9(char a, char b) { return andGate(a, notGate(a)); }
+  char opA(char a, char b) { return andGate(b, b); }
+  char opB(char a, char b) { return andGate(a, b); }
+  char opC(char a, char b) { return notGate(b); }
+  char opD(char a, char b) { return notGate(andGate(notGate(a), b)); }
+  char opE(char a, char b) { return orGate(a, b); }
+  char opF(char a, char b) { return andGate(notGate(a), b); }
+}
 
 
 void loop(){

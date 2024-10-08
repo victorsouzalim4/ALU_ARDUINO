d14bc201a1a460e8f493927ed606761093a248c8
Victor Souza Lima <victorsouzalima2064@gmail.com>
2024-10-07 21:32:46 -0300

    Folder renamed

--- VonNeumannMachine/VNM/Main.ino
+++ VonNeumannMachine/VNM/Main.ino
@@ -6,22 +6,27 @@ void setup() {
     Serial.begin(9600);
 }
 
-void loop() {
-    int i = 0;
-    while (true) {
-        if (Serial.available() >= 3) {
-            i++;
-            String x((char)Serial.read());
-            String y((char)Serial.read());
-            String w((char)Serial.read());
-            String str = x + y + w;
-            Serial.println(str);
-
-            vnm.registerOperation(str);
-            if (i >= 95) {
-                vnm.operate();
-            }
-            delay(10);
-        }
-    }
+void loop(){
+  VonNeumannMachine obj;
+  int i = 0;
+  char c;
+  while(true){ 
+  	if(Serial.available() >= 3){
+      i++;
+      String x((char)Serial.read());
+      String y((char)Serial.read());
+      String w((char)Serial.read());
+      String str = x+y+w;
+      Serial.println(str);
+  
+  		obj.registerOperation(str);
+      if(i >= 95){
+        obj.operate();
+      }
+      
+      delay(10);
+      char c = Serial.read(); // get '\n' or 'space' between expressions, DO NOT REMOVE
+	
+  	}
+  }
 }

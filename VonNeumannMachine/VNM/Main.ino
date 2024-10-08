02512d54f836244f407cb8f46fe292693577bad1
Victor Souza Lima <victorsouzalima2064@gmail.com>
2024-10-07 21:37:12 -0300

    iddentation problem fixed

--- VonNeumannMachine/VNM/Main.ino
+++ VonNeumannMachine/VNM/Main.ino
@@ -10,21 +10,20 @@ void loop(){
   int i = 0;
   char c;
   while(true){ 
-  	if(Serial.available() >= 3){
-      i++;
-      String x((char)Serial.read());
-      String y((char)Serial.read());
-      String w((char)Serial.read());
-      String str = x+y+w;
-      Serial.println(str);
-  
-  	  vnm.registerOperation(str);
-      if(i >= 95){
-        vnm.operate();
-      }
-      
-      delay(10);
-      char c = Serial.read(); // get '\n' or 'space' between expressions, DO NOT REMOVE
+  	if (Serial.available() >= 3) {
+        i++;
+        String x((char)Serial.read());
+        String y((char)Serial.read());
+        String w((char)Serial.read());
+        String str = x + y + w;
+        Serial.println(str);
+
+        vnm.registerOperation(str);
+        if (i >= 95) {
+            vnm.operate();
+        }
+        delay(10);
+        char c = Serial.read(); // get '\n' or 'space' between expressions, DO NOT REMOVE
 	
   	}
   }
